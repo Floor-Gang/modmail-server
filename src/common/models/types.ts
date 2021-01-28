534cd84e07a8a159b4e67cfad05a1871a45ef38d
Dylan Hackworth <dylhack@outlook.com>
2021-01-27 20:34:57 -0600

    Added Types

---
+++ src/common/models/types.ts
@@ -0,0 +1,30 @@
+import { Snowflake, UserFlags } from 'discord.js';
+import { Request } from 'express';
+import { Session } from 'express-session';
+
+export type User = {
+  avatar: string;
+  bot: boolean;
+  discriminator: string;
+  id: Snowflake;
+  username: string;
+  token: string;
+}
+
+export type Guild = {
+  id: string;
+  name: string;
+  icon: string;
+  owner: boolean;
+  permissions: string;
+  features: string[];
+}
+
+export interface ModmailSession extends Session {
+  user?: User;
+  guilds?: Guild[];
+}
+
+export interface RequestWithSession<a = any, b = any, c = any, d = any> extends Request<a, b, c, d> {
+  session: ModmailSession;
+}
